f88cf73ccbfd1512b19090a5858b62cb4214c47a
cuauhvip07 <cuauvillalba1@gmail.com>
2023-12-26 14:08:38 -0600

    Creacion pacientes
    Se crea los pacientes que van a estar en la lista de pacientes

--- src/components/ListaPacientes.jsx
+++ src/components/ListaPacientes.jsx
@@ -1,11 +1,14 @@
+import Pacientes from "./Pacientes"
 
 
 function ListaPacientes(){
 
     return(
-        <div>
-            <h2 className="font-black text-4xl text-center">Listado Pacientes</h2>
+        <div className="md:w-1/2 md:h-screen overflow-y-scroll">
+            <h2 className="font-black text-3xl text-center">Listado Pacientes</h2>
             <p className="mb-10 mt-5 text-center">Administra tus <span className="text-indigo-600">Pacientes y Citas</span></p>
+
+            <Pacientes />
         </div>
     )
 }
